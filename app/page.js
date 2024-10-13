6451edb92beff8060cfbec1688b28d10babe62f9
Harale Sagar <sagarharale9@gmail.com>
2024-10-13 16:44:47 +0530

    added profile and bio

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import profilePhoto from '../app/images/profilePhoto.jpg'
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <div >
      <nav className="navbar">
@@ -17,9 +27,18 @@ export default function Home() {
      <div className="content">
       <div className="about">
         <strong>Hey, I'am Sagar</strong>
+        <div className="bio-content">
+        <p>Experienced Java Fullstack Developer skilled in designing and implementing high-performing applications with a knack for both front-end and back-end technologies.</p>
+        <p>Bringing 2+ years of hands-on experience in Java development, complemented by a solid background in modern web frameworks and best practices.</p>
+        <p>Proficient in Java, Spring Boot, React, Next Js, and JSp, with a strong understanding of database management.</p>
+        <p>Committed to continuous learning and leveraging the latest tools and technologies to drive innovation and excellence.</p>
+        <p>Passionate about creating seamless user experiences and efficient, scalable software solutions.</p>
+        </div>
       </div>
       <div className="profile">
-
+      {mounted && (
+            <Image src={profilePhoto} alt="Profile Photo"/>
+          )}
       </div>
      </div>
     </div>
